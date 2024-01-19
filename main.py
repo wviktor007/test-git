0ed560cc9363cab6f55f088e8dda1d38b4b1b1b6
Viktor Trofimov <trofimov-viktor-88@Mail.ru>
2024-01-19 11:43:36 +0600

    нахождение стоимости по ключу

---
+++ main.py
@@ -0,0 +1,28 @@
+car = {
+    'BMW' : '10.000$',
+    'Subaru' : '20.000$',
+    'Lada' : '3000$',
+    'Opel' : '2000$',
+    'Haval' : '10.000$'
+}
+
+def car_price(data):
+    if car.get(data):
+        print(f'Стоимость {data} на вторичном рынке состовляет - {car[data]}')
+        all_car = input('Хотите посмотреть весь список машин? ')
+        if all_car == 'Да':
+            for k, v in car.items():
+                print(f'Стоимость {k} на вторичном рынке состовляет - {v}')
+        else:
+            print('До скорой встерчи')
+    else:
+        print('''Проверьте правильность ввода.
+Вам доступны марки:''')
+        for i in car:
+            print(i)
+
+
+
+data = input('Введите макрку машины: ')
+
+car_price(data)
